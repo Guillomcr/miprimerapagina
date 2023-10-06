caa2073541677a8227ab4a25a7499f95631eaed2
Guillomcr <147113723+Guillomcr@users.noreply.github.com>
2023-10-05 19:35:23 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Guillermo";
+nombre = "Guillo";
+
+var nombre1 = "Guillermo";
+
+const nombre2 = "Guillermo";
+//nombre2 = "Guillermo";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+// SELECCIONAR ELEMENTOS
+
+let contenidoTitulo = "Nombre";
+
+
+
+
+//CONDICIONALES
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if (textoTitulo == "Nombre"){titulo.innerHTML = "Otro";}
